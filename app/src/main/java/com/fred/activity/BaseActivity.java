442fdc9643cfe994f09499f5374e7c9bba993c18
sunxl <sunxiaoleixyz@163.com>
2016-07-18 18:37:50 +0800

    feat(创建Base页)

---
+++ app/src/main/java/com/fred/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.fred.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Administrator on 2016/7/18.
+ */
+public class BaseActivity extends FragmentActivity{
+
+}
