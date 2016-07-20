c2d38d2446a65319a62354f4008f5a19fd61949e
sunxl <sunxiaoleixyz@163.com>
2016-07-20 11:26:14 +0800

    chore(添加注释)

--- app/src/main/java/com/fred/base/BaseActivity.java
+++ app/src/main/java/com/fred/base/BaseActivity.java
@@ -16,9 +16,9 @@ public class BaseActivity extends FragmentActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MyActivityList.getInstance().addActivity(this);
-        //百度统计_设置渠道
+        //1.百度统计_设置渠道
         StatSDKService.setAppChannel(this,"channelFred1",true,"ead7b6b463");
-        //百度统计_设置日志发送延迟时间
+        //2.百度统计_设置日志发送延迟时间
         StatSDKService.setLogSenderDelayed(1,"ead7b6b463");
         LoggerManager.i(getClass().getSimpleName());
     }
@@ -26,12 +26,14 @@ public class BaseActivity extends FragmentActivity{
     @Override
     protected void onResume() {
         super.onResume();
+        //3.百度统计添加onResume
         StatSDKService.onResume(this,"ead7b6b463");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        //4.百度统计添加onPause
         StatSDKService.onPause(this,"ead7b6b463");
     }
 
