79a6ecd356e73ff0bcf67a8223fbdcb1fa0133f1
sunxl <sunxiaoleixyz@163.com>
2016-08-01 16:53:11 +0800

    feat(添加MyApplication预防DEX65535)

---
+++ app/src/main/java/com/fred/utils/MyApplication.java
@@ -0,0 +1,10 @@
+package com.fred.utils;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Administrator on 2016/7/20.
+ */
+public class MyApplication extends MultiDexApplication {
+
+}
