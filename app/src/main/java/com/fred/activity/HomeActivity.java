180635086bd760e7fa67856fc5c4fd15b175017a
sunxl <sunxiaoleixyz@163.com>
2016-08-01 17:10:20 +0800

    feat(添加腾讯bugly)

--- app/src/main/java/com/fred/activity/HomeActivity.java
+++ app/src/main/java/com/fred/activity/HomeActivity.java
@@ -14,5 +14,6 @@ public class HomeActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
--- app/src/main/java/com/fred/utils/MyApplication.java
+++ app/src/main/java/com/fred/utils/MyApplication.java
@@ -2,9 +2,15 @@ package com.fred.utils;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.tencent.bugly.crashreport.CrashReport;
+
 /**
  * Created by Administrator on 2016/7/20.
  */
 public class MyApplication extends MultiDexApplication {
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext(), "900041542", false);
+    }
 }
