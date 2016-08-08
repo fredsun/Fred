30c6bbc478f9fcaa3f09d385b8186657b81792c4
Fred <sunxiaoleixyz@163.com>
2016-08-08 16:00:50 +0800

    feat(删除百度统计)

--- app/src/main/java/com/fred/base/BaseActivity.java
+++ app/src/main/java/com/fred/base/BaseActivity.java
@@ -16,25 +16,17 @@ public class BaseActivity extends FragmentActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MyActivityList.getInstance().addActivity(this);
-        //1.百度统计_设置渠道
-        StatSDKService.setAppChannel(this,"channelFred1",true,"ead7b6b463");
-        //2.百度统计_设置日志发送延迟时间
-        StatSDKService.setLogSenderDelayed(1,"ead7b6b463");
         LoggerManager.i(getClass().getSimpleName());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //3.百度统计添加onResume
-        StatSDKService.onResume(this,"ead7b6b463");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //4.百度统计添加onPause
-        StatSDKService.onPause(this,"ead7b6b463");
     }
 
     @Override
