d2143acb62e364eeee041f31667c248c76ef7435
fred <sunxiaoleixyz@163.com>
2016-08-28 03:40:53 +0800

    撤下腾讯BUG，NDK无法模拟器测试

--- app/src/main/java/com/fred/utils/MyApplication.java
+++ app/src/main/java/com/fred/utils/MyApplication.java
@@ -2,7 +2,6 @@ package com.fred.utils;
 
 import android.support.multidex.MultiDexApplication;
 
-import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Created by Administrator on 2016/7/20.
@@ -11,6 +10,6 @@ public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        CrashReport.initCrashReport(getApplicationContext(), "900041542", false);
+
     }
 }
