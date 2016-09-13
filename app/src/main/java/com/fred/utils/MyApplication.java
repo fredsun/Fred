c40ea16d95e205b09f59597a89cfc64ccc688230
Fred <sunxiaoleixyz@163.com>
2016-09-13 16:47:33 +0800

    添加百度地图定位功能

--- app/src/main/java/com/fred/utils/MyApplication.java
+++ app/src/main/java/com/fred/utils/MyApplication.java
@@ -1,15 +1,28 @@
 package com.fred.utils;
 
+import android.app.Service;
+import android.os.Vibrator;
 import android.support.multidex.MultiDexApplication;
+
+import com.baidu.location.service.LocationService;
+import com.baidu.mapapi.SDKInitializer;
 
 
 /**
  * Created by Administrator on 2016/7/20.
  */
 public class MyApplication extends MultiDexApplication {
+    public LocationService locationService;
+    public Vibrator mVibrator;
     @Override
     public void onCreate() {
         super.onCreate();
+        /***
+         * 初始化定位sdk
+         */
+        locationService = new LocationService(getApplicationContext());
+        mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
+        SDKInitializer.initialize(getApplicationContext());
 
     }
 }
