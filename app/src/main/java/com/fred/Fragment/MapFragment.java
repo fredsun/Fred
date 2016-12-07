5715a09f031079ec47a2c3b2ac8438be527b5bba
sunxl <sunxiaoleixyz@163.com>
2016-12-07 15:16:23 +0800

    考虑完整主题, 添加Bottom_Nagivation_Bar作底

---
+++ app/src/main/java/com/fred/Fragment/MapFragment.java
@@ -0,0 +1,30 @@
+package com.fred.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fred.R;
+
+/**
+ * Created by Administrator on 2016/12/7.
+ */
+
+public class MapFragment extends Fragment {
+    public static Fragment newInstance(String name){
+        Fragment fragment = new MapFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("fragmentName",name);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map,container,false);
+    }
+}
--- app/src/main/java/com/fred/activity/HomeActivity.java
+++ app/src/main/java/com/fred/activity/HomeActivity.java
@@ -2,18 +2,102 @@ package com.fred.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.fred.Fragment.ChatFragment;
+import com.fred.Fragment.GameFragment;
+import com.fred.Fragment.HomeFragment;
+import com.fred.Fragment.MapFragment;
+import com.fred.Fragment.MusicFragment;
 import com.fred.R;
 import com.fred.base.BaseActivity;
+
+import java.util.ArrayList;
 
 /**
  * Created by Fred on 2016/9/18.
  */
-public class HomeActivity extends BaseActivity{
+public class HomeActivity extends BaseActivity implements BottomNavigationBar.OnTabSelectedListener {
+    private ArrayList<Fragment> fragments;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.btm_home);
+        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC
+                );
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.mipmap.ic_music_note_white_24dp, "Music").setActiveColorResource(R.color.blue))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_location_on_white_24dp, "Map").setActiveColorResource(R.color.teal))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_home_white_24dp, "Home").setActiveColorResource(R.color.orange))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_link_white_24dp, "Chat").setActiveColorResource(R.color.brown))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_videogame_asset_white_24dp, "Games").setActiveColorResource(R.color.grey))
+                .setFirstSelectedPosition(0)
+                .initialise();
+
+        fragments = getFragments();
+        setDefaultFragment();
+        bottomNavigationBar.setTabSelectedListener(this);
+    }
+    /** * 设置默认的 */
+    private void setDefaultFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.fl_home, HomeFragment.newInstance("Home"));
+        transaction.commit();
+    }
+
+    private ArrayList<Fragment> getFragments() {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(MusicFragment.newInstance("Music"));
+        fragments.add(MapFragment.newInstance("Map"));
+        fragments.add(HomeFragment.newInstance("Home"));
+        fragments.add(ChatFragment.newInstance("Chat"));
+        fragments.add(GameFragment.newInstance("Games"));
+        return fragments;
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        if (fragments != null) {
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+                if (fragment.isAdded()) {
+                    ft.replace(R.id.fl_home, fragment);
+                } else {
+                    ft.add(R.id.fl_home, fragment);
+                }
+                ft.commitAllowingStateLoss();
+            }
+        }
 
     }
+
+    @Override
+    public void onTabUnselected(int position) {
+        if (fragments != null) {
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+                ft.remove(fragment);
+                ft.commitAllowingStateLoss();
+            }
+        }
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
+    }
+
+
 }
